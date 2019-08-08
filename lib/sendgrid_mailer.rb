e852e7703062f8dc6127f03dc575f11e8fdaa775
Navin Peiris <navinpeiris@gmail.com>
2019-08-08 16:32:57 +0530

    Initial setting of defaults and email attributes

--- lib/sendgrid_mailer.rb
+++ lib/sendgrid_mailer.rb
@@ -1,4 +1,91 @@
 require 'sendgrid_mailer/version'
 
+require 'sendgrid-ruby'
+
 class SendGridMailer
+  include SendGrid
+
+  def self.defaults
+    @defaults || {}
+  end
+
+  def self.default(args)
+    @defaults ||= {}
+    @defaults.merge!(args)
+  end
+
+  def self.method_missing(method, *args)
+    if instance_methods.include?(method.to_sym)
+      new.method(method).call(*args)
+    else
+      super
+    end
+  end
+
+  def self.respond_to_missing?(method_name, include_private = false)
+    instance_methods.include?(method.to_sym) || super
+  end
+
+  def mail(template_id: nil,
+           from: nil,
+           from_name: nil,
+           subject: nil)
+    self.template_id = template_id if template_id
+    self.from = from if from
+    self.from_name = from_name if from_name
+    self.subject = subject if subject
+
+    self
+  end
+
+  def template_id
+    sg_mail.template_id
+  end
+
+  def template_id=(template_id)
+    sg_mail.template_id = template_id
+  end
+
+  def from
+    sg_mail.from['email']
+  end
+
+  def from=(email)
+    sg_mail.from = Email.new(email: email, name: from_name)
+  end
+
+  def from_name
+    sg_mail.from['name']
+  end
+
+  def from_name=(name)
+    sg_mail.from = Email.new(email: from, name: name)
+  end
+
+  def subject
+    sg_mail.subject
+  end
+
+  def subject=(subject)
+    sg_mail.subject = subject
+  end
+
+  def sg_mail
+    @sg_mail ||= begin
+      mail = SendGrid::Mail.new
+
+      mail.template_id = defaults[:template_id]
+      mail.from = Email.new(email: defaults.fetch(:from, 'no-reply@example.com'),
+                            name: defaults[:from_name])
+      mail.subject = defaults[:subject]
+
+      mail
+    end
+  end
+
+  private
+
+  def defaults
+    self.class.defaults
+  end
 end
--- spec/sendgrid_mailer_spec.rb
+++ spec/sendgrid_mailer_spec.rb
@@ -1,5 +1,50 @@
 RSpec.describe SendGridMailer do
-  it 'has a version number' do
-    expect(SendGridMailer::VERSION).not_to be nil
+  class DefaultsMailer < SendGridMailer
+    default template_id: 'template-id'
+    default from: 'notifications@example.com', from_name: 'Example Notifier'
+    default subject: 'Hello from example'
+  end
+
+  it 'sets email fields from provided defaults' do
+    mail = DefaultsMailer.mail
+
+    expect(mail).not_to be nil
+
+    expect(mail.template_id).to eql 'template-id'
+    expect(mail.from).to eql 'notifications@example.com'
+    expect(mail.from_name).to eql 'Example Notifier'
+    expect(mail.subject).to eql 'Hello from example'
+  end
+
+  it 'can override defaults through mail method' do
+    mail = DefaultsMailer.mail template_id: 'new-template-id',
+                               from: 'two@example.com',
+                               from_name: 'Two',
+                               subject: 'Subject Two'
+
+    expect(mail.template_id).to eql 'new-template-id'
+    expect(mail.from).to eql 'two@example.com'
+    expect(mail.from_name).to eql 'Two'
+    expect(mail.subject).to eql 'Subject Two'
+  end
+
+  it 'can override just the email' do
+    mail = DefaultsMailer.mail(from: 'two@example.com')
+
+    expect(mail.from).to eql 'two@example.com'
+
+    expect(mail.template_id).to eql 'template-id'
+    expect(mail.from_name).to eql 'Example Notifier'
+    expect(mail.subject).to eql 'Hello from example'
+  end
+
+  it 'can override just the name' do
+    mail = DefaultsMailer.mail(from_name: 'Me Two')
+
+    expect(mail.from_name).to eql 'Me Two'
+
+    expect(mail.template_id).to eql 'template-id'
+    expect(mail.from).to eql 'notifications@example.com'
+    expect(mail.subject).to eql 'Hello from example'
   end
 end
